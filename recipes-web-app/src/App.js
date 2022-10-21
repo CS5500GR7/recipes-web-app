631090b4ded7d9a76bfcabc8473b4ff8aeb1f8cb
Chao Pan <77814127+panchaoneu@users.noreply.github.com>
2022-10-20 17:31:12 -0700

    app

--- recipes-web-app/src/App.js
+++ recipes-web-app/src/App.js
@@ -1,5 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/HomeScreen/home-screen.css';
+import SearchScreen from "./components/Search/search-screen";
+import HomeScreen from "./components/HomeScreen/home-screen";
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import Profile from "./components/Profile/profile";
+import ProfileAdmin from "./components/Profile/profile-admin";
+import DetailsScreen from "./components/Details/detail-screen";
+import Login from "./components/User/login";
+import Privacy from "./components/PrivacyPolicy/privacy"
+import Register from "./components/User/register";
+import AboutUs from "./components/AboutUs";
+import Others from "./components/Others";
+import React, {Fragment, useEffect, useState} from 'react';
+import userService from "./services/user-service";
 
 function App() {
   return (
