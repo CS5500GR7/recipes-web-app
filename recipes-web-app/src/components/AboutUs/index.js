8e58447c7f96f3c08a43e69d6af7a8e5a50704a7
RachelLiu6 <ruixiliu6@gmail.com>
2022-11-05 15:45:53 -0700

    Add AboutUS - index.js & index.css

---
+++ recipes-web-app/src/components/AboutUs/index.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from "react";
+import "./index.css";
+
+const AboutUs = () => {
+    return (
+        <Fragment>
+            <p>
+                The Cocktail Time is a destination where every cocktail enthusiast can explore our curated collection of incredible cocktails and learn how to make them. Try a new spirit or put your own unique spin on one of the hundreds of drinks you find. Discover the art of cocktails by learning a new technique “Behind the Bar,” or just let the stunning, colorful drink images inspire your next big occasion or casual evening at home. Search hundreds of recipes for every spirit type, from sweet Tequila to smoky Bourbon. There’s something for everyone, from Margaritas to Martinis, party punches to seasonal favorites, old-fashioned classics to the latest mixologist concoctions.
+            </p>
+
+            <p>
+                Register for free and save your favorite recipes, or create a shopping list for your next event. While you’re here, share your cocktail picks with your friends & fellow cocktail lovers. Keep coming back to see the latest additions to our growing list of recipes and new bartending tips. We promise you’ll find an amazing cocktail every time you visit.
+            </p>
+
+        </Fragment>
+
+    );
+}
+export default AboutUs;
