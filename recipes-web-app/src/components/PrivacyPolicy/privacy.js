aba186406c1452adf430a88ad8140798e0281fbe
RachelLiu6 <ruixiliu6@gmail.com>
2022-11-05 16:45:29 -0700

    Add Privacy Policy - privacy.js & privacy.css

---
+++ recipes-web-app/src/components/PrivacyPolicy/privacy.js
@@ -0,0 +1,114 @@
+import "./privacy.css"
+
+const Privacy = () => {
+    return (
+        <>
+            <h1>Privacy Policy</h1>
+            <p>Last updated: November 5, 2022</p>
+            <p>This Privacy Policy describes Drink Masters policies and procedures on the collection, use and disclosure of Your
+                information when You use the Service and tells You about Your privacy rights and how the law protects
+                You.</p>
+            <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the
+                collection and use of information in accordance with this Privacy Policy. </p>
+
+            <h3>Collecting and Using Your Personal Data</h3>
+            <h4>Types of Data Collected</h4>
+            <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information
+                that
+                can be used to contact or identify You. Personally identifiable information may include, but is not
+                limited
+                to:</p>
+            <ul>
+                <li>
+                    <p>Email</p>
+                </li>
+                <li>
+                    <p>First name and last name</p>
+                </li>
+                <li>
+                    <p>Phone number</p>
+                </li>
+                <li>
+                    <p>Address</p>
+                </li>
+                <li>
+                    <p>Usage Data*</p>
+                </li>
+            </ul>
+            <p>*Usage Data is collected automatically when using the Service.</p>
+            <p>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address),
+                browser
+                type, browser version, the pages of our Service that You visit, the time and date of Your visit, the
+                time
+                spent on those pages, unique device identifiers and other diagnostic data.</p>
+            <p>When You access the Service by or through a mobile device, We may collect certain information
+                automatically,
+                including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP
+                address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You
+                use,
+                unique device identifiers and other diagnostic data.</p>
+            <p>We may also collect information that Your browser sends whenever You visit our Service or when You access
+                the
+                Service by or through a mobile device.</p>
+
+            <h4>Use of Your Data</h4>
+            <p>The Company may use Personal Data for the following purposes:</p>
+            <ul>
+                <li>
+                    <p><strong>To provide and maintain our Service</strong>, including to monitor the usage of our
+                        Service.
+                    </p>
+                </li>
+                <li>
+                    <p><strong>To manage Your Account:</strong> to manage Your registration as a user of the Service.
+                        The
+                        Personal Data You provide can give You access to different functionalities of the Service that
+                        are
+                        available to You as a registered user.</p>
+                </li>
+                <li>
+                    <p><strong>For the performance of a contract:</strong> the development, compliance and undertaking
+                        of
+                        the purchase contract for the products, items or services You have purchased or of any other
+                        contract with Us through the Service.</p>
+                </li>
+                <li>
+                    <p><strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other
+                        equivalent
+                        forms of electronic communication, such as a mobile application's push notifications regarding
+                        updates or informative communications related to the functionalities, products or contracted
+                        services, including the security updates, when necessary or reasonable for their implementation.
+                    </p>
+                </li>
+                <li>
+                    <p><strong>To provide You</strong> with news, special offers and general information about other
+                        goods,
+                        services and events which we offer that are similar to those that you have already purchased or
+                        enquired about unless You have opted not to receive such information.</p>
+                </li>
+                <li>
+                    <p><strong>To manage Your requests:</strong> To attend and manage Your requests to Us.</p>
+                </li>
+
+                <li>
+                    <p><strong>For other purposes</strong>: We may use Your information for other purposes, such as data
+                        analysis, identifying usage trends, determining the effectiveness of our promotional campaigns
+                        and
+                        to evaluate and improve our Service, products, services, marketing and your experience.</p>
+                </li>
+            </ul>
+            <h3>Changes to this Privacy Policy</h3>
+            <p>We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new
+                Privacy Policy on this page.</p>
+            <p>We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming
+                effective and update the "Last updated" date at the top of this Privacy Policy.</p>
+            <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy
+                Policy
+                are effective when they are posted on this page.</p>
+            <h3>Contact Us</h3>
+            <p>If you have any questions about this Privacy Policy, You can contact us:</p>
+            <p>By email: drinkmasters@webdev.com</p>
+
+        </>)
+}
+export default Privacy;
