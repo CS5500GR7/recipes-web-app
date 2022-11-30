77aded2469a6cefa87e067c1957933160446d379
yihongwang001 <yihongwang001@outlook.com>
2022-11-29 19:17:23 -0800

    add unit test for about us - second paragraph

--- recipes-web-app/src/aboutUs.test.js
+++ recipes-web-app/src/aboutUs.test.js
@@ -1,11 +1,16 @@
 import AboutUs from "./components/AboutUs/index";
 test("Test about-us ", () => {
-    let aboutUS = "The Drink Masters is a destination where every cocktail enthusiast can explore our curated collection" +
-        " of incredible cocktails and learn how to make them. Try a new spirit or put your own unique spin on one of the" +
-        " hundreds of drinks you find. Discover the art of cocktails by learning a new technique “Behind the Bar,” " +
+    let aboutUsFirstParagraph = "The Drink Masters is a destination where every cocktail enthusiast can explore our " +
+        "curated collection of incredible cocktails and learn how to make them. Try a new spirit or put your own unique " +
+        "spin on one of the hundreds of drinks you find. Discover the art of cocktails by learning a new technique “Behind the Bar,” " +
         "or just let the stunning, colorful drink images inspire your next big occasion or casual evening at home. " +
         "Search hundreds of recipes for every spirit type, from sweet Tequila to smoky Bourbon. There’s something " +
         "for everyone, from Margaritas to Martinis, party punches to seasonal favorites, old-fashioned classics to " +
         "the latest mixologist concoctions.";
-    expect(AboutUs()['props']['children'][0]['props']['children']).toBe(aboutUS)
+    let aboutUsSecondParagraph = "Register for free and save your favorite recipes, or create a shopping list for your " +
+        "next event. While you’re here, share your cocktail picks with your friends & fellow cocktail lovers. Keep coming" +
+        " back to see the latest additions to our growing list of recipes and new bartending tips. We promise you’ll find" +
+        " an amazing cocktail every time you visit.";
+    expect(AboutUs()['props']['children'][0]['props']['children']).toBe(aboutUsFirstParagraph)
+    expect(AboutUs()['props']['children'][1]['props']['children']).toBe(aboutUsSecondParagraph)
 });
